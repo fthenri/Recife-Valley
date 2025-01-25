75563c04aee652dc4762f127cc5f0ae70758f826
Henrique Figueiredo <hft@cesar.school>
2025-01-24 23:24:02 -0300

    day one messing around

--- main.c
+++ main.c
@@ -26,6 +26,8 @@ int main()
 
     InitWindow(screenWidth, screenHeight, "raylib");
 
+    Vector2 ballPosition = {(float)screenWidth/2, (float)screenHeight/2};
+
     //--------------------------------------------------------------------------------------
     SetTargetFPS(60);               // Set our game to run at 60 frames-per-second
     //--------------------------------------------------------------------------------------
@@ -33,7 +35,19 @@ int main()
     // Main game loop
     while (!WindowShouldClose())    // Detect window close button or ESC key
     {
+        // Update
+        //----------------------------------------------------------------------------------
+        if (IsKeyDown(KEY_RIGHT)) ballPosition.x += 2.0f;
+        if (IsKeyDown(KEY_LEFT)) ballPosition.x -= 2.0f;
+        if (IsKeyDown(KEY_UP)) ballPosition.y -= 2.0f;
+        if (IsKeyDown(KEY_DOWN)) ballPosition.y += 2.0f;
+        //----------------------------------------------------------------------------------
         BeginDrawing();
+            ClearBackground(RAYWHITE);
+
+            DrawText("move the ball with arrow keys", 10, 10, 20, DARKGRAY);
+
+            DrawCircleV(ballPosition, 10, MAROON);
         EndDrawing(); 
     }
 
